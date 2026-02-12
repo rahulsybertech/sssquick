8c066a663540e8977192927c4c320e0268707ccb
monu5855 <monu@ssssybertech.com>
2026-02-12 12:52:32 +0530

    Stock in office total amt show

---
+++ app/src/main/java/com/ssspvtltd/quick/utils/RemoteConfigManager.kt
@@ -0,0 +1,27 @@
+package com.ssspvtltd.quick.utils
+
+object RemoteConfigManager {
+    private const val TAG = "RemoteConfigManager"
+    private const val BASE_URL_KEY = "base_url"
+
+    fun initConfig(onComplete: (String) -> Unit) {
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600 // Fetch once per hour
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+
+        // Set default value in case of no internet
+        remoteConfig.setDefaultsAsync(mapOf(BASE_URL_KEY to "https://appapi.ssspltd.com/"))
+
+        remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+            val url = if (task.isSuccessful) {
+                remoteConfig.getString(BASE_URL_KEY)
+            } else {
+                remoteConfig.getString(BASE_URL_KEY) // Fallback to default
+            }
+            Log.d(TAG, "Fetched URL: $url")
+            onComplete(url)
+        }
+    }
+}
